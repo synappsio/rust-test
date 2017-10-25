e1df630dbf890e93a66752532ac08adda3a98237
Vincent Wiart <vincent.wiart@gmail.com>
2017-10-25 15:30:01 +0200

    cargo

---
+++ printable.rs
@@ -0,0 +1,21 @@
+/*use std::fmt;
+
+struct User{
+    id: i32,
+    name: str,
+}
+
+impl User {
+    pub fn new(id: i32, name: str) -> User {
+        User{id, name}
+    }
+}*/
+
+fn main() {
+    /*let name = "Vinz";
+    let id = 1337;
+
+    let vinz = User{id, name};
+
+    println!("{:?}", vinz);*/
+}
